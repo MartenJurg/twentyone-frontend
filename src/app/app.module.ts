afbda0c2774ba75552989abce84fc8d860db7cbd
mjurge <mjurge@ttu.ee>
2019-10-11 14:08:09 +0300

    Router setup. Connecting login, register and home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes :Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path:"",
+    component: HomeComponent,
+    pathMatch: "full"
+  },
+  {
+    path:"**",
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
