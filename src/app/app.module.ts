d4509990354a29a128f27a0079fd2fcec86f7109
mjurge <Amkisnuim1>
2019-11-15 21:24:59 +0200

    Fixed style and deleted house.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from "@angular/common/http";
+import { FormsModule} from "@angular/forms";
+import {Router, RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import {Router, RouterModule, Routes} from "@angular/router";
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule} from "@angular/common/http";
 import { AuthComponent } from './auth/auth.component';
-import { FormsModule} from "@angular/forms";
 import { DataComponent } from './data/data.component';
 import { ToGoComponent } from './to-go/to-go.component';
-import { HouseDataComponent } from './house-data/house-data.component';
-
 import { BoxingClubComponent } from './boxing-club/boxing-club.component';
 import { StreetsComponent } from './streets/streets.component';
 import { KitchenComponent } from './kitchen/kitchen.component';
 import { PubComponent } from './pub/pub.component';
 import { CraftingRoomComponent } from './crafting-room/crafting-room.component';
+import { HouseComponent } from './house/house.component';
 
 const appRoutes :Routes = [
   {
@@ -72,12 +71,12 @@ const appRoutes :Routes = [
     AuthComponent,
     DataComponent,
     ToGoComponent,
-    HouseDataComponent,
     BoxingClubComponent,
     StreetsComponent,
     KitchenComponent,
     PubComponent,
     CraftingRoomComponent,
+    HouseComponent,
   ],
   imports: [
     BrowserModule,
