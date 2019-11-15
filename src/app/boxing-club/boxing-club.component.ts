842a0ba4b7488434e173c472e3c58e1cdba3bc09
mjurge <mjurge@ttu.ee>
2019-11-15 18:51:12 +0200

    Trying to fix pipeline errors

--- src/app/boxing-club/boxing-club.component.ts
+++ src/app/boxing-club/boxing-club.component.ts
@@ -12,6 +12,7 @@ export class BoxingClubComponent implements OnInit {
   constructor(private skillService: SkillingService, private tokenService: TokenStorageService) { }
 
   ngOnInit() {
+
   }
 
 
