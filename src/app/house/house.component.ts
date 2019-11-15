ddf907ead0dbaa453a25bfafa75fb17761ff8542
mjurge <Amkisnuim1>
2019-11-15 21:37:25 +0200

    Fixed data displaying

--- src/app/house/house.component.ts
+++ src/app/house/house.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TokenStorageService} from "../auth/token-storage.service";
 
 @Component({
   selector: 'app-house',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HouseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+    this.tokenStorage.updateDataAndInventory();
   }
 
 }
